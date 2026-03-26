096706d75069bb185e28b4ea2dc1716484d65270
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-03-26 19:10:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,39 @@
-variable "ami" {}
-variable "instance_type" {}
-variable "subnet_id" {}
+variable "ami" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
+}
 
 variable "key_name" {
   description = "EC2 key pair"
+  type        = string
 }
 
 variable "security_group_ids" {
-  type = list(string)
+  description = "List of SG IDs"
+  type        = list(string)
 }
 
 variable "public_ip" {
-  type    = bool
-  default = true
+  description = "Assign public IP"
+  type        = bool
+  default     = true
 }
 
-variable "instance_name" {}
+variable "instance_name" {
+  description = "Base name of instance"
+  type        = string
+}
+
 variable "environment" {
   description = "Environment name (dev/test/prod)"
   type        = string
