fc7b98e6b721bc58d23979433eec6f210550ab47
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-03-26 11:00:52 +0530

    new modified file

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
 resource "aws_instance" "ec2" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  key_name               = var.key_name
+  vpc_security_group_ids = var.security_group_ids
+
+  associate_public_ip_address = var.public_ip
 
   tags = {
-    Name = "my-ec2"
+    Name        = var.instance_name
+    Environment = var.environment
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,19 @@
 variable "ami" {}
 variable "instance_type" {}
 variable "subnet_id" {}
+
+variable "key_name" {
+  description = "EC2 key pair"
+}
+
+variable "security_group_ids" {
+  type = list(string)
+}
+
+variable "public_ip" {
+  type    = bool
+  default = true
+}
+
+variable "instance_name" {}
+variable "environment" {}
